a2742240de141b6b9fb09654c863efae5c236bb8
j-jinjia <j.jinjia99@gmail.com>
2022-02-21 12:24:16 +0100

    Trying another approach

--- script.js
+++ script.js
@@ -4,20 +4,15 @@ import pokemon from "./data/pokemon.js"
 const pokemonCards = document.querySelector(".card-container");
 console.log(pokemon);
 
-pokemon.renderCards();{
-    `<div class ="card">
-    <div class ="card__image"> </div>
-        <img src =${this.sprite}</img>
-    </div>
-    <div class ="card__content">
-        <h3 class="card__heading"> ${this.name}</h3>
-        <p class="card__text">${this.name}( ${this.id}) is a ${this.type[0]} & ${this.type[1]} type pokemon.</p>
-    </div>
 
-</div>`;
+const pokeCards = pokemon.map((pokemons) => {
+    const pokemonReturnObject = {
+        id: pokemons.id,
+        name: pokemons.name,
+        types: pokemons.types,
+        sprite: pokemons.sprite,
 
-
-
-}   
-
-pokemonCards.innerHTML=pokemon.renderCards();
+    }
+    return pokemonReturnObject
+});
+pokemonCards.innerHTML += pokeCards
