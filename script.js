4afd3dd987990c33f08c600d768fed3f3bbdcb98
j-jinjia <j.jinjia99@gmail.com>
2022-02-21 21:10:54 +0100

    Final push

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ import pokemonArray from "./data/pokemon.js"
 //DOM element for HTML 
 const pokemonCards = document.querySelector(".card-container");
 
+/** Looping through the pokemonArray and adding each element into the new array pokemonCards by changing the innerHTML.
+ * New HTML tags added to creat the cards. 
+ * 
+ * Used ${element.name.charAt(0).toUpperCase()+element.name.slice(1) to make the first letter of the name Uppercase. and then add the rest of the name in lowercase. 
+ * 
+ * Used ${element.types.join(" & ")} to concatenate array elements with a separator, in this case the & symbol.
+ */
+
 pokemonArray.forEach(element => 
     pokemonCards.innerHTML += 
     `<div class ="card">
         <img src =${element.sprite} class ="card__image"></img>
         <div class ="card__content">
-            <h3 class="card__heading"> ${element.name}</h3>
-            <p class="card__text">${element.name} #( ${element.id}) is a ${element.types[0]} & ${element.types[1]} type pokemon.</p>
+            <h3 class="card__heading"> ${element.name.charAt(0).toUpperCase()+element.name.slice(1)}</h3>
+            <p class="card__text">${element.name.charAt(0).toUpperCase()+element.name.slice(1)} (#${element.id}) is a ${element.types.join(" & ")} type pokemon.</p>
     </div>
 
     </div>`
