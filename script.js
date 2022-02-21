a7fe74597ce961cf122f07daedf8ec9804f3bf1d
j-jinjia <j.jinjia99@gmail.com>
2022-02-21 20:43:56 +0100

    Something's showing on screen

--- script.js
+++ script.js
@@ -2,17 +2,14 @@
 import pokemon from "./data/pokemon.js"
 //DOM element for HTML 
 const pokemonCards = document.querySelector(".card-container");
-console.log(pokemon);
 
 
-const pokeCards = pokemon.map((pokemons) => {
-    const pokemonReturnObject = {
-        id: pokemons.id,
-        name: pokemons.name,
-        types: pokemons.types,
-        sprite: pokemons.sprite,
+pokemonCards.innerHTML = 
+`<div class ="card">
+    <img src =${pokemon.sprite} class ="card__image"></img>
+    <div class ="card__content">
+        <h3 class="card__heading"> ${pokemon.name}</h3>
+        <p class="card__text">${pokemon.name}( ${pokemon.id}) is a ${pokemon.type} & ${pokemon.type} type pokemon.</p>
+    </div>
 
-    }
-    return pokemonReturnObject
-});
-pokemonCards.innerHTML += pokeCards
+</div>`
