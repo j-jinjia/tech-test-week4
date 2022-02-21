c4b00c777eb7e86c2e190eb8882662dc2d0375ff
j-jinjia <j.jinjia99@gmail.com>
2022-02-21 20:56:50 +0100

    Cards are showing correctly

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //Import pokemonArray from another file.
-import pokemon from "./data/pokemon.js"
+import pokemonArray from "./data/pokemon.js"
 //DOM element for HTML 
 const pokemonCards = document.querySelector(".card-container");
 
-
-pokemonCards.innerHTML = 
-`<div class ="card">
-    <img src =${pokemon.sprite} class ="card__image"></img>
-    <div class ="card__content">
-        <h3 class="card__heading"> ${pokemon.name}</h3>
-        <p class="card__text">${pokemon.name}( ${pokemon.id}) is a ${pokemon.type} & ${pokemon.type} type pokemon.</p>
+pokemonArray.forEach(element => 
+    pokemonCards.innerHTML += 
+    `<div class ="card">
+        <img src =${element.sprite} class ="card__image"></img>
+        <div class ="card__content">
+            <h3 class="card__heading"> ${element.name}</h3>
+            <p class="card__text">${element.name} #( ${element.id}) is a ${element.types[0]} & ${element.types[1]} type pokemon.</p>
     </div>
 
-</div>`
+    </div>`
+
+)
